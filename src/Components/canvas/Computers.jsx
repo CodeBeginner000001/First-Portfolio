ba2f86e7cd3982c67924624a2118468b558da196
Adarsh <ashu2100ag@gmail.com>
2024-10-08 09:30:43 +0530

    Completed 40% of work

--- src/Components/canvas/Computers.jsx
+++ src/Components/canvas/Computers.jsx
@@ -1,6 +1,6 @@
 import {Suspense,useEffect,useState} from 'react'
 import {Canvas} from '@react-three/fiber'
-import {OrbitControls,Preload,SpotLight,useGLTF} from "@react-three/drei"
+import {OrbitControls,Preload,useGLTF} from "@react-three/drei"
 import CanvasLoader from "../Loader";
 const Computers = ({isMobile}) => {
   const computer = useGLTF("./desktop_pc/scene.gltf")
