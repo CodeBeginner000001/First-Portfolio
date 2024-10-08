e773ae4e2b55ffa57b463d191b2e600beddb4f2f
Adarsh <ashu2100ag@gmail.com>
2024-10-08 22:48:52 +0530

    started build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Example: All node_modules code will be bundled in a 'vendor' chunk
+          }
+        }
+      }
+    }
+  }
 })
