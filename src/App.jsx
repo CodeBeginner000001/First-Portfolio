2c91b73bce24f6c56ebad3797232c877a37185f9
Adarsh <ashu2100ag@gmail.com>
2024-10-08 23:44:58 +0530

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         </div>
         <About />
         <Experience />
-        {/* <Tech /> */}
+        <Tech />
         <Works />
         <Feedbacks />
         <div className="relative z-0">
