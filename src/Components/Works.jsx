480107482c4f7f55bba752bdcc09c922f1379014
SoftwareProject000001 <ashu2100ag@gmail.com>
2025-03-17 16:25:43 +0530

    Update Works.jsx

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -65,7 +65,8 @@ const Works = () => {
           Projects.
         </h2>
       </motion.div>
-      <motion.p
+      <div className='w-full flex'>
+        <motion.p
       variants={fadeIn("","",0.1,1)}
       className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
@@ -73,6 +74,7 @@ const Works = () => {
         described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work
         with different technologies, and manage projects effectively.
       </motion.p>
+      </div>
 
       <div className="mt-20 flex flex-wrap gap-7 justify-center">
         {projects.map((project,index)=>(
