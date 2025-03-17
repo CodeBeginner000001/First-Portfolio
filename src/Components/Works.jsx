e8c8840ca7320204da23b79d6bfdf5986330c90a
SoftwareProject000001 <ashu2100ag@gmail.com>
2025-03-17 17:29:00 +0530

    Update Works.jsx

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -11,6 +11,8 @@ const ProjectCard = ({index,name,description,tags,image,source_code_link,link})=
   return (
     <motion.div
     variants={fadeIn("up","spring",index*0.5,0.75)}
+initial="hidden" 
+  animate="show"
     >
       <Tilt
       options={{
